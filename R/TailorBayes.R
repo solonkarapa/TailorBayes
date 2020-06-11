196e565f22bb5c9bdc930f2444f9de503242a7d8
Solon Karapanagiotis <karapanagiotissolon@gmail.com>
2020-06-11 09:34:43 +0100

    update metrop_tailor doc

--- R/TailorBayes.R
+++ R/TailorBayes.R
@@ -10,7 +10,7 @@
 #' It returns a list object where the first element, \code{chain}, is of class "mcmc". This element can be summarized by
 #' functions provided by the coda package or similar ones. See function doc for details.
 #'
-#' The \code{ESS_t()} functions calculates the effective sample size for tailoring, i.e., the sample size
+#' The \code{ESS_t()} function calculates the effective sample size for tailoring, i.e., the sample size
 #' inferences will be based on.
 #'
 #' @docType package
